bda45f3d1a7c0513596b007de29047be76f90e92
LIU Wei <liuwei@nbu.edu.cn>
2018-03-13 13:13:08 +0800

    Finish Controller with CRUD

---
+++ Models/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace LearningAspNetCoreWithElementUI.Models {
+    public class TodoItem {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
